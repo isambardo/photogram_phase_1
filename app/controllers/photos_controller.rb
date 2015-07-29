e9ee362f4a45c7535bbe466a02d4079546b9147f
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-07-29 12:01:46 -0500

    adds display photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,15 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @list_of_photos = Photo.all
+    @selected_photo = params["id"]
+    @list_of_photos.each do |selector|
+      if @selected_photo.to_i == selector.id
+         @display_me = selector.source
+         @use_this_caption = selector.caption
+      end
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 end
+
